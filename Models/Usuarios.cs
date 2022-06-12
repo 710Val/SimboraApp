8c42cb319874a0b7602aa45114527598a83aceb8
User <User@DESKTOP-9RGK65T>
2022-06-11 22:53:54 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Usuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simbora04.Models
+{
+    [Table("Usuarios")]
+    public class Usuarios
+    {
+		[Key]
+		public int Id { get; set; }
+
+		[Display(Name = "Email")]
+		[Required(ErrorMessage = "Obrigatório informar o email")]
+		public string Nome { get; set; }
+
+		[Display(Name = "Senha")]
+		[Required(ErrorMessage = "Obrigatório informar a senha")]
+		public string Senha { get; set; }
+
+	}
+}
